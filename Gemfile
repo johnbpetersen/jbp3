ff3d792e0c4d85d1312b6045e142de5f3291d853
johnbpetersen <john.b.petersen@gmail.com>
2012-10-29 17:51:54 -0400

    initial commit

---
+++ Gemfile
@@ -0,0 +1,19 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.8'
+gem 'sqlite3'
+gem "haml-rails"
+gem 'jquery-rails'
+gem "inherited_resources"
+gem 'bcrypt-ruby', '~> 3.0.0'
+
+
+# Gems used only for assets and not required
+# in production environments by default.
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem "twitter-bootstrap-rails"
+  gem 'uglifier', '>= 1.0.3'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jbp3::Application.config.secret_token = '5c938143b0c740f2cd1f6627f5bd53469e75bd4f6a2496acf2b906ef7f659e3cc381276fd19c9c663807826cc9bd4175ccea691cf97201003eb52cbf0fcff7c5'
